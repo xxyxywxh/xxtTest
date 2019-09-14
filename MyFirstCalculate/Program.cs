b56bdb11862a359666c693b1cd6da063caa52a4c
Administrator <Administrator@DESKTOP-DG83DVD>
2019-09-14 09:41:44 +0800

    我的项目

---
+++ MyFirstCalculate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("我的计算器");
+        Console.WriteLine("=======================");
+        inputNum01:
+        Console.Write("请输入第一个数字：");
+        string str1 = Console.ReadLine();
+        if (str1.Trim().Length == 0)
+        {
+            Console.WriteLine("输入的不是数字！");
+            goto inputNum01;
+        }
+        int Num1 = Convert.ToInt32(str1);
+        if (Num1 < 0 || Num1 > 1000)
+        {
+            Console.WriteLine("输入的数字超出范围！");
+            goto inputNum01;
+        }
+
+        inputNum02:
+        Console.Write("请输入第二个数字：");
+        string str2 = Console.ReadLine();
+        if (str2.Trim().Length == 0)
+        {
+            Console.WriteLine("输入的不是数字！");
+            goto inputNum02;
+        }
+        int Num2 = Convert.ToInt32(str2);
+        if (Num2 < 0 || Num2 > 1000)
+        {
+            Console.WriteLine("输入的数字超出范围！");
+            goto inputNum02;
+        }
+
+        Console.WriteLine("计算结果:{0} + {1} = {2}", Num1, Num2, Num1 + Num2);
+        Console.WriteLine("计算完毕！");
+        Console.ReadKey();
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtL2CacheSize = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtL3CacheSize = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumOfCore = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNumOfProcess = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCodeCollection = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(437, 458);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(429, 432);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "处理器";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(429, 432);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "缓存";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(429, 432);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "主板";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(429, 432);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "内存";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.txtCodeCollection);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtNumOfProcess);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtNumOfCore);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtL3CacheSize);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtL2CacheSize);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(3, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(420, 193);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "处理器";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "名字";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.SystemColors.Control;
+            this.txtName.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtName.Location = new System.Drawing.Point(64, 23);
+            this.txtName.Name = "txtName";
+            this.txtName.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(209, 21);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtL2CacheSize
+            // 
+            this.txtL2CacheSize.BackColor = System.Drawing.SystemColors.Control;
+            this.txtL2CacheSize.Enabled = false;
+            this.txtL2CacheSize.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtL2CacheSize.Location = new System.Drawing.Point(64, 50);
+            this.txtL2CacheSize.Name = "txtL2CacheSize";
+            this.txtL2CacheSize.Size = new System.Drawing.Size(209, 21);
+            this.txtL2CacheSize.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "二级缓存";
+            // 
+            // txtL3CacheSize
+            // 
+            this.txtL3CacheSize.BackColor = System.Drawing.SystemColors.Control;
+            this.txtL3CacheSize.Enabled = false;
+            this.txtL3CacheSize.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtL3CacheSize.Location = new System.Drawing.Point(64, 77);
+            this.txtL3CacheSize.Name = "txtL3CacheSize";
+            this.txtL3CacheSize.Size = new System.Drawing.Size(209, 21);
+            this.txtL3CacheSize.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "三级缓存";
+            // 
+            // txtNumOfCore
+            // 
+            this.txtNumOfCore.BackColor = System.Drawing.SystemColors.Control;
+            this.txtNumOfCore.Enabled = false;
+            this.txtNumOfCore.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtNumOfCore.Location = new System.Drawing.Point(64, 104);
+            this.txtNumOfCore.Name = "txtNumOfCore";
+            this.txtNumOfCore.Size = new System.Drawing.Size(209, 21);
+            this.txtNumOfCore.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "核心数";
+            // 
+            // txtNumOfProcess
+            // 
+            this.txtNumOfProcess.BackColor = System.Drawing.SystemColors.Control;
+            this.txtNumOfProcess.Enabled = false;
+            this.txtNumOfProcess.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtNumOfProcess.Location = new System.Drawing.Point(64, 131);
+            this.txtNumOfProcess.Name = "txtNumOfProcess";
+            this.txtNumOfProcess.Size = new System.Drawing.Size(209, 21);
+            this.txtNumOfProcess.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "线程数";
+            // 
+            // txtCodeCollection
+            // 
+            this.txtCodeCollection.BackColor = System.Drawing.SystemColors.Control;
+            this.txtCodeCollection.Enabled = false;
+            this.txtCodeCollection.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtCodeCollection.Location = new System.Drawing.Point(64, 158);
+            this.txtCodeCollection.Name = "txtCodeCollection";
+            this.txtCodeCollection.Size = new System.Drawing.Size(209, 21);
+            this.txtCodeCollection.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "指令集";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(285, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(343, 403);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 482);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtCodeCollection;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNumOfProcess;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNumOfCore;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtL3CacheSize;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtL2CacheSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
